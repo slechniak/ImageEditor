6b76f7e219983b7e7ccbe8b98bfbb1defe3137f1
slechniak <szymonlechniak@gmail.com>
2022-04-27 23:24:08 +0200

    pan, detecting windowDPR change

--- PracaInz04/Client/wwwroot/js/scripts.js
+++ PracaInz04/Client/wwwroot/js/scripts.js
@@ -8,3 +8,20 @@
 function windowDevicePixelRatio() {
     return window.devicePixelRatio;
 }
+
+class WindowDPRHelper {
+    static dotNetHelper;
+
+    static callupdatePixelRatio(value) {
+        WindowDPRHelper.dotNetHelper = value;
+        WindowDPRHelper.updatePixelRatio();
+    }
+
+    static updatePixelRatio = () => {
+        let pr = window.devicePixelRatio;
+        matchMedia(`(resolution: ${pr}dppx)`).addEventListener("change", WindowDPRHelper.updatePixelRatio, { once: true });
+        WindowDPRHelper.dotNetHelper.invokeMethodAsync('SetWindowDPR', pr);
+    }
+}
+
+window.WindowDPRHelper = WindowDPRHelper;
